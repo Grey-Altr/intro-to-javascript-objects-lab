285269b12715f53bd998024e677744535ff1b9ec
GreyA <grey.altaer@gmail.com>
2025-01-26 15:58:36 -0800

    exercised 1-3 complete

--- app.js
+++ app.js
@@ -18,3 +18,39 @@ const game = {
     { name: "rare candy", quantity: 99 },
   ],
 };
+
+
+/*
+Exercise 3
+1. Add a new property to the `game` object. Let's call it "difficulty".
+2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+
+
+Solve Exercise 3 here:
+*/
+
+
+game.difficulty = [
+    'Easy',
+    'Medium',
+    'Hard',
+];
+
+
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
+
+
+Solve Exercise 4 here:
+*/
+
+
+const selectStarterMonToParty = () => {
+    
+}
+
+//console.dir(pokemon, { maxArrayLength: null });
+
+console.log(game);
